e12c7aee7907101845117aad72df93f7904dc549
Mbarek Matis <mbarekmatis@gmail.com>
2025-11-18 10:45:03 +0100

    feat: supprimer le contrôleur et le service de l'application dans le service de commande

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { PrismaService } from 'prisma/prisma.service';
 import { HealthController } from './health.controller';
@@ -8,6 +7,6 @@ import { HealthController } from './health.controller';
 @Module({
   imports: [ConfigModule.forRoot(), OrdersModule],
   controllers: [HealthController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
